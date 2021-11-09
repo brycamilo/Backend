b7f8095541a30d0e523b698fb45264a3b497601a
brycamilo <brycamilo@gmail.com>
2021-11-09 11:44:31 -0500

    001 Servidor instalado

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require ('cors')
+const bodyparser = require ('body-parser')
+
+
+app.set('Port',4000)
+
+app.use(morgan('dev'))
+app.use(bodyparser.urlencoded({extended:true}))
+app.use (bodyparser.json)
+app.use (cors({origen:"*"}))
+
+
+app.listen(app.get ( 'Port'),()=>{
+    console.log('servidor escuchando por el puertooo',app.get('Port'))
+})
